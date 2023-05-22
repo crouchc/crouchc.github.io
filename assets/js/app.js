46db4982c96c8791d7d502c3a7e67ba54a092843
Crouch <crouchc@ufl.edu>
2023-05-22 10:39:27 -0400

    finished project list

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 function toggleProject(projectIndex) {
+
   // Get the project info elements
   const projectInfoElements = document.querySelectorAll('.project-info');
+
   // Hide all project info elements
   projectInfoElements.forEach(element => {
     element.style.display = 'none';
   });
+
   // Show the project info element corresponding to the clicked project
   const clickedProjectInfo = document.querySelector('#project-info-' + (projectIndex + 1));
   clickedProjectInfo.style.display = 'block';
